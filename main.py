d30abefd3139350cf21bf07275651e8f46fb879a
oxymususer <oxymususer@gmail.com>
2024-11-12 16:14:48 +0500

    added editing and finding by keywords notes

--- main.py
+++ main.py
@@ -17,9 +17,12 @@ connection.commit() # save changes
 
 # get arguments for app
 parser = argparse.ArgumentParser()
-parser.add_argument("-l", "--list", action="store_true")
-parser.add_argument("-c", "--create", action="store_true")
-parser.add_argument("-d", "--delete", type=int)
+parser.add_argument("-l", "--list", action="store_true", help="show list of notes")
+parser.add_argument("-c", "--create", action="store_true", help="create new note")
+parser.add_argument("-d", "--delete", type=int, metavar="ID", help="delete note by id")
+parser.add_argument("-en", "--edit-name", type=int, metavar="ID", help="edit name note by id")
+parser.add_argument("-et", "--edit-text", type=int, metavar="ID", help="edit text note by id")
+parser.add_argument("-f", "--find", nargs="+", metavar="KEYWORDS", help="find note")
 
 args = parser.parse_args()
 
@@ -32,7 +35,7 @@ if args.list:
 		for note in notes:
 			print("{0}. {1}".format(note[0], note[1]))
 			print("Text: " + note[2])
-			print("\n\n")
+			print("\n")
 	else:
 		print("The list is empty")
 elif args.create:
@@ -46,13 +49,11 @@ elif args.create:
 	while True:
 		try:
 			line = input()
+			textNote += line + "\n"
 		except EOFError:
 			break
-		textNote += line + "\n"
 
-	cursor.execute('''
-		INSERT INTO `notes` (name, text) VALUES (?, ?)
-	''', (nameNote, textNote))
+	cursor.execute("INSERT INTO `notes` (name, text) VALUES (?, ?)", (nameNote, textNote))
 
 	connection.commit() # save changes
 elif args.delete is not None:
@@ -60,5 +61,50 @@ elif args.delete is not None:
 	cursor.execute("DELETE FROM `notes` WHERE id = ?", (args.delete,))
 
 	connection.commit() # save changes
+elif args.edit_name is not None:
+	# edit name note
+	nameNote = input("Input new note name: ")
+
+	cursor.execute("UPDATE `notes` SET name = ? WHERE id = ?", (nameNote, args.edit_name))
+
+	connection.commit() #save changes
+elif args.edit_text is not None:
+	# edit text note
+	print("Input updated text note (Ctrl + D or Ctrl + Z to save it):")
+	textNote = ""
+
+	# input multiline text
+	while True:
+		try:
+			line = input()
+			textNote += line + "\n"
+		except EOFError:
+			break
+
+	cursor.execute("UPDATE `notes` SET text = ? WHERE id = ?", (textNote, args.edit_text))
+
+	connection.commit() # save changes
+elif args.find is not None:
+	# find a notes by keyword
+	cursor.execute("SELECT * FROM `notes`")
+	notes = cursor.fetchall()
+
+	i = 0
+
+	# print notes which has substring
+	for note in notes:
+		print("keyword: {0}".format(args.find[i]))
+		print("---------------------------------------------------------------")
+
+		for keyword in args.find:
+			if note[2].find(keyword) == -1:
+				continue
+
+			print("{0}. {1}".format(note[0], note[1]))
+			print("Text: " + note[2])
+			print("\n")
+
+		print("===============================================================")
+		i += 1
 
 connection.close() # close connection
